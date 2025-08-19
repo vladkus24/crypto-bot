fbe3eebfaaeb4e8b3e3202eb87ebaf441b7bef83
monKey <58446879+vladkus24@users.noreply.github.com>
2025-08-19 10:06:17 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+# Telegram
+TELEGRAM_BOT_TOKEN = "8397905643:AAGd9LIPyl2R0VN-V1cd6DmH0JqAitz0iDw"
+CHANNEL_ID = -1003083822026
+
+# Helius API
+HELIUS_API_KEY = "e9298291-93b7-42ac-93ae-682da47133b2"
+HELIUS_URL = f"https://mainnet.helius-rpc.com/?api-key={HELIUS_API_KEY}"
+
+# Birdeye API (бесплатный ключ можно взять на https://birdeye.so)
+BIRDEYE_API_KEY = "demo"   # <-- сюда потом вставишь свой ключ
+BIRDEYE_URL = "https://public-api.birdeye.so/defi"
+
+# Настройки
+MIN_BUY_AMOUNT_SOL = 1       # минимум SOL для покупки
+MIN_WALLETS_TRIGGER = 3      # от скольки разных кошельков слать сигнал
